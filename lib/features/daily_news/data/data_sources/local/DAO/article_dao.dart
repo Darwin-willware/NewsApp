17d6e762983d843b429e4adba817af90f56f2b38
Darwin-willware <144683078+Darwin-willware@users.noreply.github.com>
2023-11-27 18:05:19 +0530

    Add files via upload

---
+++ lib/features/daily_news/data/data_sources/local/DAO/article_dao.dart
@@ -0,0 +1,16 @@
+import 'package:floor/floor.dart';
+import 'package:news_app/features/daily_news/data/models/article.dart';
+
+
+@dao
+abstract class ArticleDao {
+
+  @Insert()
+  Future<void> inserArticle(ArticleModel article);
+
+  @delete
+  Future<void> deleteArticle(ArticleModel articleModel);
+
+  @Query('SELECT * FROM article')
+  Future<List<ArticleModel>> getArticles();
+}
